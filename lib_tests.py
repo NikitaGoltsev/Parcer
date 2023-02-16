c43e16abed68b9ad2678a46efa333203dbf7424b
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-02-16 13:20:30 +0700

    Start_Of_Proj

---
+++ lib_tests.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+
+
+class Func_From_BS():
+    def __init__(self) -> None:
+        
+        return None
+
+    def Work_Check(self):
+        BeautifulSoup("<a></p>", "html5lib")
+        return None
+
+
+main_obj = Func_From_BS()
+main_obj.Work_Check()
---
+++ main.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+
+class Main_Class():
+    def __init__(self) -> None:
+        
+        return None
